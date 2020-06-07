bcad3f312271b1b2838d3c013c60dd9a436fadae
Hello-ChenLiYan <891626779@qq.com>
2020-06-07 20:54:04 +0800

    表和ID注解

---
+++ src/main/java/com/first/annotation/Table.java
@@ -0,0 +1,17 @@
+package com.first.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+;
+
+/**
+ * @author 小胖
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Table {
+    String value() default "";
+}
